6073040c25308a6eb64a6b7289b08d88fb1ceb9b
Esmeralda Marin <esmemarinm03@gmail.com>
2021-03-18 20:23:08 -0300

    loader

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+let body = document.querySelector('body')
+
+window.onload = () => {
+    let ctn = document.querySelector('.loader')
+    ctn.style.visibility = "hidden";
+    ctn.style.opacity= "0";
+}
