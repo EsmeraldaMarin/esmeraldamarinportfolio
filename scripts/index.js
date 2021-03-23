64c545cbc95c068fb099eac98eb63e6adb27a258
Esmeralda Marin <esmemarinm03@gmail.com>
2021-03-22 22:21:47 -0300

    burguer menu

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,12 @@
 let body = document.querySelector('body')
+let burguerBtn = document.querySelector('.burguerMenu')
 
 window.onload = () => {
     let ctn = document.querySelector('.loader')
     ctn.style.visibility = "hidden";
     ctn.style.opacity= "0";
 }
+
+burguerBtn.addEventListener('click', ()=>{
+    burguerBtn.classList.toggle('menuActive')
+})
