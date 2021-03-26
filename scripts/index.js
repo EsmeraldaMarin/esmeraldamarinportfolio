a65c18de3d7611701e7ec4ac6e3f2cb05ae60626
Esmeralda Marin <esmemarinm03@gmail.com>
2021-03-26 12:28:44 -0300

    header

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
 let body = document.querySelector('body')
 let burguerBtn = document.querySelector('.burguerMenu')
-
+/* 
 window.onload = () => {
     let ctn = document.querySelector('.loader')
     ctn.style.visibility = "hidden";
     ctn.style.opacity = "0";
     body.style.overflow = "auto";
-}
+} */
 
 burguerBtn.addEventListener('click', () => {
     burguerBtn.classList.toggle('menuActive')
